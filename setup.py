a9cd4c2138dc4796195ae1995b416d2f76c85382
Kevin Johnson <appliedmathkj@gmail.com>
2019-03-06 09:22:59 -0800

    version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ long_description = 'An Opinionated AlphaVantage API Wrapper in Python 3.7'
 
 setup(
     name='alphaVantage_api',
-    version='1.0.14',
+    version='1.0.15',
     description=long_description,
     long_description=long_description,
     author='Kevin Johnson',
